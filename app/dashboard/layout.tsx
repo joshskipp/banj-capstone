d659e4fbbd647efce6b85a616c9a0d2d41a23cfd
Skipp <85834248+joshskipp@users.noreply.github.com>
2025-03-12 00:09:03 +1000

    Cleaned up after merge (login with commod)

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -9,21 +9,41 @@ export default async function Layout({ children }: { children: React.ReactNode }
     const user = await CurrentUser();
   return (
         <div>
-          <div className="container mx-auto px-4  justify-between items-start ">
-            <div className="ml-0 flex items-start space-x-4">
-                {/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
-                <Link href="/dashboard" className=" bg-gray-200 px-3 py-2 text-sm font-medium border-b-[#7a232a] border-b-4 hover:underline hover:underline-offset-4"  aria-current="page">Dashboard</Link>
-                <Link href="/dashboard/commodities" className=" px-3 py-2 text-sm font-medium hover:underline hover:underline-offset-4">Commodities</Link>
-                <a href="#" className=" px-3 py-2 text-sm font-medium hover:underline hover:underline-offset-4">link 2</a>
-                <a href="#" className=" px-3 py-2 text-sm font-medium hover:underline hover:underline-offset-4">link 3</a>
-                <a href="#" className=" px-3 py-2 text-sm font-medium hover:underline hover:underline-offset-4">link 4</a>
-                <Link href="/dashboard" className=" bg-gray-200 px-3 py-2 text-sm font-medium border-b-[#7a232a] border-b-4 hover:underline hover:underline-offset-4"  aria-current="page">Dashboard</Link>
-                {/*<a href="#" className=" px-3 py-2 text-sm font-medium hover:underline hover:underline-offset-4">link 1</a>*/}
-                {/*<a href="#" className=" px-3 py-2 text-sm font-medium hover:underline hover:underline-offset-4">link 2</a>*/}
-                {/*<a href="#" className=" px-3 py-2 text-sm font-medium hover:underline hover:underline-offset-4">link 3</a>*/}
-                <Link href="/dashboard/settings" className=" px-3 py-2 text-sm font-medium hover:underline hover:underline-offset-4">Settings</Link>
+            <div className="container flex flex-row mx-auto px-4 items-start border-b-[#7a232a] gap-2.5">
+                <Link
+                    href="/dashboard"
+                    className="bg-gray-200 px-3 py-2 text-sm font-medium border-b-[#7a232a] border-b-4 hover:underline hover:underline-offset-4"
+                    aria-current="page">
+                    Dashboard
+                </Link>
+                <Link
+                    href="#"
+                    className="px-3 py-2 text-sm font-medium hover:bg-gray-200 hover:underline hover:underline-offset-4 hover:border-b-[#7a232a] hover:border-b-4">
+                    Projects(*)
+                </Link>
+                <Link
+                    href="/dashboard/commodities"
+                    className="px-3 py-2 text-sm font-medium hover:bg-gray-200 hover:underline hover:underline-offset-4 hover:border-b-[#7a232a] hover:border-b-4">
+                    Commodities
+                </Link>
+                <Link
+                    href="#"
+                    className="px-3 py-2 text-sm font-medium hover:bg-gray-200 hover:underline hover:underline-offset-4 hover:border-b-[#7a232a] hover:border-b-4">
+                    Companies(*)
+                </Link>
+                <Link
+                    href="#"
+                    className="px-3 py-2 text-sm font-medium hover:bg-gray-200 hover:underline hover:underline-offset-4 hover:border-b-[#7a232a] hover:border-b-4">
+                    Key Events(*)
+                </Link>
+
+                <Link
+                    href="/dashboard/settings"
+                    className="px-3 py-2 ml-auto text-sm font-medium hover:bg-gray-200 hover:underline hover:underline-offset-4 hover:border-b-[#7a232a] hover:border-b-4">
+                    Settings
+                </Link>
+
             </div>
-          </div>
           <main className="container mx-auto px-4 py-4">
           {children}
           </main>
