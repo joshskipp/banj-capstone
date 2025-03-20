d3d2d6873eab8f376777e832c66d5030c4f05b7a
Skipp <85834248+joshskipp@users.noreply.github.com>
2025-03-20 10:32:47 +1000

    Refactor nav bar into reusable component with dynamic links
    - Extract nav bar into standalone component
    - Implement dynamic rendering from link objects (name, href, icon)
    - Update styling

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,54 +1,15 @@
 //import SideNav from "@/app/ui/dashboard/sidenav-template";
 
-import Link from 'next/link';
+import NavBar from "@/app/ui/dashboard/nav-bar";
 
 export default async function Layout({ children }: { children: React.ReactNode }) {
   return (
         <div>
             <div className="container flex flex-row mx-auto px-4 items-start border-b-[#7a232a] gap-2.5">
-                <Link
-                    href="/dashboard"
-                    className="bg-gray-200 px-3 py-2 text-sm font-medium border-b-[#7a232a] border-b-4 hover:underline hover:underline-offset-4"
-                    aria-current="page">
-                    Dashboard
-                </Link>
-                <Link
-                    href="/dashboard/projects/"
-                    className="px-3 py-2 text-sm font-medium hover:bg-gray-200 hover:underline hover:underline-offset-4 hover:border-b-[#7a232a] hover:border-b-4">
-                    Projects
-                </Link>
-                <Link
-                    href="/dashboard/commodities"
-                    className="px-3 py-2 text-sm font-medium hover:bg-gray-200 hover:underline hover:underline-offset-4 hover:border-b-[#7a232a] hover:border-b-4">
-                    Commodities
-                </Link>
-                <Link
-                    href="/dashboard/companies"
-                    className="px-3 py-2 text-sm font-medium hover:bg-gray-200 hover:underline hover:underline-offset-4 hover:border-b-[#7a232a] hover:border-b-4">
-                    Companies
-                </Link>
-                <Link
-                    href="#"
-                    className="px-3 py-2 text-sm font-medium hover:bg-gray-200 hover:underline hover:underline-offset-4 hover:border-b-[#7a232a] hover:border-b-4">
-                    Key Events(*)
-                </Link>
-
-                <Link
-                    href="/dashboard/settings"
-                    className="px-3 py-2 ml-auto text-sm font-medium hover:bg-gray-200 hover:underline hover:underline-offset-4 hover:border-b-[#7a232a] hover:border-b-4">
-                    Settings
-                </Link>
-
+                <NavBar />
             </div>
           <main className="container mx-auto px-4 py-4">
           {children}
           </main>
-
-
-          <footer className="bg-[#1f4656] text-white min-h-48 border-t-[#7a232a] border-t-8">
-            <div className="container px-4 py-1">
-            qld.gov.au
-            </div>
-          </footer>
           </div>
   );}
