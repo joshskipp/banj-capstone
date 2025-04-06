249a69b73b3ef17a30d8e65d9f518b9f8fd04a8f
Skipp <85834248+joshskipp@users.noreply.github.com>
2025-04-06 16:42:29 +1000

    feature: add new users
    Form now allows the creation of new users. Note, permissions do nothing. If passwords don't match, the system throws and error.

--- app/ui/settings/new-user-form.tsx
+++ app/ui/settings/new-user-form.tsx
@@ -1,99 +1,87 @@
 'use client'
+import { createUser } from "@/app/lib/writedb/write-users"
 
-import {AtSymbolIcon, KeyIcon, UserIcon} from "@heroicons/react/24/outline";
 
 export default function NewUserForm() {
     return (
-        <form className="p-3 mt-3 border-gray-200 rounded-lg border-2">
+        <form className="p-3 mt-3 border-gray-200 rounded-lg border-2" action={createUser}>
             <h2>Add User</h2>
-            <div className="w-full space-y-2">
+            <div className="grid grid-cols-2 gap-1">
                 <div>
-                    <label
-                        className="block text-xs font-medium text-gray-900"
-                        htmlFor="name"
-                    >
-                        Name
-                    </label>
-                    <div className="relative">
-                        <input
-                            className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-                            id="name"
-                            type="text"
-                            name="text"
-                            placeholder="Enter User's Name"
-                            required
-                        />
-                        <UserIcon
-                            className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900"/>
-                    </div>
+                    <label htmlFor="name">Name</label>
+                    <input
+                        id="name"
+                        type="text"
+                        name="name"
+                        placeholder="Enter User's Name"
+                        required
+                    />
                 </div>
                 <div>
-                    <label
-                        className="block text-xs font-medium text-gray-900"
-                        htmlFor="email"
-                    >
-                        Email
-                    </label>
-                    <div className="relative">
-                        <input
-                            className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-                            id="email"
-                            type="email"
-                            name="email"
-                            placeholder="Enter your email address"
-                            required
-                        />
-                        <AtSymbolIcon
-                            className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900"/>
-                    </div>
+                    <label htmlFor="email">Email</label>
+                    <input
+                        id="email"
+                        type="email"
+                        name="email"
+                        placeholder="Enter your email address"
+                        required
+                    />
                 </div>
+
                 <div className="">
-                    <label
-                        className="block text-xs font-medium text-gray-900"
-                        htmlFor="password"
-                    >
-                        Password
-                    </label>
-                    <div className="relative">
-                        <input
-                            className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-                            id="password"
-                            type="password"
-                            name="password"
-                            placeholder="Enter password"
-                            required
-                            minLength={6}
-                        />
-                        <KeyIcon
-                            className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900"/>
-                    </div>
-                    <label
-                        className="block text-xs font-medium text-gray-900"
-                        htmlFor="password"
-                    >
-                        Confirm Password
-                    </label>
-                    <div className="relative">
-                        <input
-                            className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-                            id="password-confirm"
-                            type="password"
-                            name="password-confirm"
-                            placeholder="Enter password again"
-                            required
-                            minLength={6}
-                        />
-                        <KeyIcon
-                            className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900"/>
-                    </div>
-                    <div className="flex row space-x-2 pt-4">
-                    <button type="reset" className="w-1/8 border-gray-400 border-[1px] p-2 text-xs rounded-sm" aria-disabled='false'>
+                    <label htmlFor="password">Password</label>
+                    <input
+                        id="password"
+                        type="password"
+                        name="password"
+                        placeholder="Enter password"
+                        required
+                        minLength={6}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="passwordConfirm">Confirm Password</label>
+                    <input
+                        id="passwordConfirm"
+                        type="password"
+                        name="passwordConfirm"
+                        placeholder="Enter password again"
+                        required
+                        minLength={6}
+                    />
+                </div>
+
+                <div>
+                    <fieldset>
+                        <legend>Permissions</legend>
+                        <div className="grid grid-cols-4 gap-1">
+                            <div className="flex items-center gap-1.5">
+                                <input type="checkbox" id="review" name="permissions" defaultChecked/>
+                                <label htmlFor="review">Review</label>
+                            </div>
+
+                            <div className="flex items-center gap-1.5">
+                                <input type="checkbox" id="analyst" name="permissions" defaultChecked/>
+                                <label htmlFor="analyst">Analyst</label>
+                            </div>
+                            <div className="flex items-center gap-1.5">
+                                <input type="checkbox" id="admin" name="permissions" />
+                                <label htmlFor="admin">Admin</label>
+                            </div>
+                        </div>
+                    </fieldset>
+                </div>
+
+                <div className="flex row space-x-2">
+                    <button type="reset" className="w-1/8 border-gray-400 border-[1px] p-2 text-xs rounded-sm"
+                            aria-disabled='false'>
                         Clear Form
                     </button>
-                    <button type="submit" disabled className="w-1/4 border-[1px] bg-blue-400 p-2 text-xs font-bold text-white rounded-sm hover:cursor-not-allowed" aria-disabled='true'>
+                    <button type="submit"
+                            className="w-1/4 border-[1px] bg-blue-400 p-2 text-xs font-bold text-white rounded-sm"
+                            aria-disabled='false'>
                         Create User
                     </button>
-                    </div>
                 </div>
             </div>
         </form>
