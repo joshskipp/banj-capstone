bf5ed7c486fc2ae512ef759b80c5acc70b16bb83
bjhughes9 <n10769803@qut.edu.au>
2025-05-16 16:31:25 +1000

    adding fields

--- app/ui/keyevents/edit-keyevent.tsx
+++ app/ui/keyevents/edit-keyevent.tsx
@@ -11,7 +11,7 @@ export default function Form(
             <h2>Editing event for: {keyevent.project_name}</h2>
             <fieldset>
                 <legend>Required</legend>
-                {/*<input type="hidden" id="user_id" name="user_id" value={user_id} />*/}
+                
                 {/*<label>Project</label>*/}
                 {/*<select id="project_id" name="project_id">*/}
                 {/*    {projOptions.map((p) => (*/}
@@ -19,11 +19,17 @@ export default function Form(
                 {/*    ))}*/}
                 {/*</select>*/}
 
+                <label>Event Name</label>
+                <input type='text' id={"event_name"} name={"event_name"} />
+
                 <label>Event Date</label>
                 <input type="date" id="event_date" name="event_date" defaultValue={initalDate} required className={"w-fit"}></input>
 
                 <label>Event Details</label>
                 <textarea id={"event_details"} defaultValue={keyevent.event_details} name={"event_details"}>
+
+                <label>Source/URL</label>
+                <input type='text' id={"event_source"} name={"event_source"}/>
 
                 </textarea>
             </fieldset>
