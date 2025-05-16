f625eb1e52d989800073f440d9a7a32dbd19e8d4
bjhughes9 <n10769803@qut.edu.au>
2025-05-16 16:59:15 +1000

    fixing linking of key events from Project Details page

--- app/ui/keyevents/edit-keyevent.tsx
+++ app/ui/keyevents/edit-keyevent.tsx
@@ -8,7 +8,7 @@ export default function Form(
     const initalDate = (keyevent.event_date).toISOString().split('T')[0]
     return (
         <form action={updateEventByID} >
-            <h2>Editing event for: {keyevent.project_name}</h2>
+            <h2>Editing event {keyevent.event_name} for Project: {keyevent.project_name}</h2>
             <fieldset>
                 <legend>Required</legend>
                 
@@ -20,7 +20,7 @@ export default function Form(
                 {/*</select>*/}
 
                 <label>Event Name</label>
-                <input type='text' id={"event_name"} name={"event_name"} />
+                <input type='text' id={"event_name"} defaultValue={keyevent.event_name} name={"event_name"} />
 
                 <label>Event Date</label>
                 <input type="date" id="event_date" name="event_date" defaultValue={initalDate} required className={"w-fit"}></input>
@@ -29,11 +29,11 @@ export default function Form(
                 <textarea id={"event_details"} defaultValue={keyevent.event_details} name={"event_details"}></textarea>
 
                 <label>Source/URL</label>
-                <input type='text' id={"event_source"} name={"event_source"}/>
+                <input type='text' id={"event_source"} defaultValue={keyevent.event_source} name={"event_source"}/>
                 
             </fieldset>
 
-            <button type="submit" className="fluent-primary-button">Create KeyEvent</button>
+            <button type="submit" className="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition-colors">Edit Key Event</button>
         </form>
     )
 }
